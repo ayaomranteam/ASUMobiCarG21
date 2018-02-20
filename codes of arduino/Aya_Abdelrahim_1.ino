59a6227d739550e4537ed6ca5c087d731396357b
AyaAbdelrahimAbdelnabyEbaid <36312587+AyaAbdelrahimAbdelnabyEbaid@users.noreply.github.com>
2018-02-20 19:46:31 +0200

    Add files via upload

--- codes of arduino/Aya_Abdelrahim_1.ino
+++ codes of arduino/Aya_Abdelrahim_1.ino
@@ -1,30 +1,13 @@
-int ledpins[] ={4,5,6,7};
+const int LED = 13;
 void setup() {
   // put your setup code here, to run once:
-for(int index=0;index<4;index++)
-
-  pinMode (ledpins[index],OUTPUT);
+pinMode(LED,OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-for(int k=0;k<4;k++);
-{
-  if(k==3)
-  {
-    for(int x=3;x>0;x--)
-    {
-      digitalWrite (ledpins[x],HIGH);
-      delay(1000);
-      digitalWrite (ledpins[x],LOW);
-      delay(1000);
-    }
-  }
-
-  digitalWrite (ledpins[k],HIGH);
-  delay(1000);
-  digitalWrite (ledpins[k],LOW);
-  delay(1000);
-}
-  
+ digitalWrite(LED,HIGH);
+ delay(500);
+ digitalWrite(LED,LOW);
+ delay(1500);
 }
