f0914bacc75e0a9de8283ac28c2d426c45185ca2
tokahossam <36389374+tokahossam@users.noreply.github.com>
2018-02-21 22:55:54 +0200

    Add files via upload

---
+++ codes of arduino/EX1-LED_WITH_SWITCH.ino
@@ -0,0 +1,22 @@
+//led with button
+ int ledPin = 12;
+ int buttonPin = 2;
+int value;
+void setup(){
+  pinMode(ledPin , OUTPUT);
+pinMode(buttonPin , INPUT);}
+void loop(){value=digitalRead(buttonPin);
+if(value==HIGH)
+{digitalWrite(ledPin,HIGH);
+delay(1000);
+digitalWrite(ledPin,LOW);
+delay(1000);}
+else{digitalWrite(ledPin,LOW);}}
+
+
+
+
+
+
+
+
