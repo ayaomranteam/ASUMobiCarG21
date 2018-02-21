84612c5eb20971b6976e4bd1e6558b81f24c8369
tokahossam <36389374+tokahossam@users.noreply.github.com>
2018-02-21 22:53:12 +0200

    Add files via upload

---
+++ codes of arduino/S1-q2.ino
@@ -0,0 +1,30 @@
+int timer = 1000;           
+int ledPins[] = { 
+  4,5,6,7 };       
+int pinCount = 4;           
+
+void setup() {
+  
+  for (int thisPin = 0; thisPin < pinCount; thisPin++)  {
+    pinMode(ledPins[thisPin], OUTPUT);      
+  }
+}
+
+void loop() {
+    for (int thisPin = 0; thisPin < pinCount; thisPin++) { 
+   
+    digitalWrite(ledPins[thisPin], HIGH);   
+    delay(timer);                
+        digitalWrite(ledPins[thisPin], LOW);    
+
+  }
+
+  
+  for (int thisPin = pinCount - 1; thisPin >= 0; thisPin--) { 
+    
+    digitalWrite(ledPins[thisPin], HIGH);
+    delay(timer);
+    
+    digitalWrite(ledPins[thisPin], LOW);
+  }
+}
