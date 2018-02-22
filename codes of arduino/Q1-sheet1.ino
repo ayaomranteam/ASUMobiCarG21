3d1efc2329eeacb11cadbfbd213cfb8392c631e4
ayaislam <36310868+ayaislam@users.noreply.github.com>
2018-02-22 02:47:06 +0200

    Add files via upload

---
+++ codes of arduino/Q1-sheet1.ino
@@ -0,0 +1,12 @@
+int led =0;
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite (led,HIGH);
+delay(1000);
+digitalWrite (led,LOW);
+delay(1000);
+}
