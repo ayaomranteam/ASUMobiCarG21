83e78b7e046066b7fd07c28b9530356e49a07acf
ayaislam <36310868+ayaislam@users.noreply.github.com>
2018-02-22 03:18:03 +0200

    Add files via upload

---
+++ codes of arduino/q4-sheet2.ino
@@ -0,0 +1,22 @@
+int j=0;
+int i=0;
+int led =8;
+int button=0;
+int signalvalue=0;
+void setup() {
+  // put your setup code here, to run once:
+pinMode (button,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+for(i=0;i<6;i++)
+{button=digitalRead (7);
+if(button==HIGH)
+j++;
+signalvalue =analogRead(j);}
+digitalWrite (led,HIGH);
+delay(signalvalue);
+digitalWrite (led,LOW);
+delay(signalvalue);
+}
