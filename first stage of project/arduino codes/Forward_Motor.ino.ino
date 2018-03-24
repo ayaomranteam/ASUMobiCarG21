10d2377eeace725df372dd18af7b8f2724a41985
AyaAbdelrahimAbdelnabyEbaid <36312587+AyaAbdelrahimAbdelnabyEbaid@users.noreply.github.com>
2018-03-24 22:26:13 +0200

    Add files via upload

---
+++ first stage of project/arduino codes/Forward_Motor.ino.ino
@@ -0,0 +1,41 @@
+
+int motorControllA = 4;
+int motorControllB = 7;
+int motorControllC = 8;
+int motorControllD = 12;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println(">> START<<");  
+  
+  pinMode(motorControllA, OUTPUT);
+  pinMode(motorControllB, OUTPUT);
+  pinMode(motorControllC, OUTPUT);
+  pinMode(motorControllD, OUTPUT);
+}
+
+void loop() {
+  if(Serial.available() > 0)
+  {
+    char data;
+    data = Serial.read();
+    Serial.write(Serial.read());
+
+    switch (data)
+    {
+      
+      case 'F': //FORWARD
+          digitalWrite(motorControllA, HIGH);
+          digitalWrite(motorControllB, LOW);
+          digitalWrite(motorControllC, HIGH);
+          digitalWrite(motorControllD, LOW);
+        break;
+        
+   
+    }
+  }
+}
+
+
